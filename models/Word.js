c667861f4949b21941c09fedecf60fa9129c6a8e
Gala <martin.peigercz@gmail.com>
2024-12-28 16:59:44 +0100

    Removed unnecessary models folder

--- models/Word.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const wordSchema = new mongoose.Schema({
-    word: { type: String, required: true },
-    translation: { type: String, required: true },
-    categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true }
-});
-
-module.exports = mongoose.model('Word', wordSchema);
